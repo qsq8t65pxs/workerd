3dcaf0b518318e4da0cc9f010ecacacc4e645cfd
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-04-17 21:09:47 +0900

    Set Pyodide 314.0.0a1 as a development version

--- src/pyodide/internal/pool/esbuild.config.mjs
+++ src/pyodide/internal/pool/esbuild.config.mjs
@@ -6,6 +6,8 @@ const pyodideRootDir = dirname(
   dirname(dirname(fileURLToPath(import.meta.url)))
 );
 
+const pyodideVersion = '%PYODIDE_VERSION%';
+
 let resolvePlugin = {
   name: 'pyodide-internal',
   setup(build) {
@@ -14,15 +16,13 @@ let resolvePlugin = {
       let rest = args.path.split(':')[1];
       let path;
       if (rest.startsWith('generated')) {
-        // I couldn't figure out how to pass down the version, so instead we'll look through the
-        // directories in `pyodideRootDir` and find one that starts with a 0. This will work until
-        // Pyodide has a 1.0 release.
-        const dir = readdirSync(pyodideRootDir).filter((x) =>
-          x.startsWith('0')
-        )[0];
-        path = join(pyodideRootDir, dir, rest);
+        path = join(pyodideRootDir, pyodideVersion, rest);
         if (!existsSync(path)) {
-          path += '.js';
+          if (existsSync(path + '.js')) {
+            path += '.js';
+          } else {
+            path += '.mjs';
+          }
         }
       } else {
         path = join(pyodideRootDir, 'internal', rest);
